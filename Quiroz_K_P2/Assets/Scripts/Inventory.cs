087bc59da6325b1f0e011fdfce9841dbc97a5354
karizmaquiroz <quiroz.karizma@gmail.com>
2025-02-26 03:06:19 -0500

    Scripts Inventory & Item
    Not done with the inventory scripts:
     -needs completion (commented lines)
     -needs guis for inventory storage & quickItem

--- Quiroz_K_P2/Assets/Scripts/Inventory.cs
+++ Quiroz_K_P2/Assets/Scripts/Inventory.cs
@@ -1,16 +1,133 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor.Search;
 
 public class Inventory : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    bool showInventoryMenu = false;
+    public Rect inventoryRect;// = new Rect(Screen.width / 2, Screen.height / 2, 400, 400);
+    public GameObject EmptyObject;
+    private int InventorySize = 12;
+
+
+
+
+    //public Item[] inventoryItems; //
+    public List<Item> inventoryItems;
+
+    //public Item[] QuickItems; //
+    public List<Item> QuickItems;
+    List<Item> Items = new List<Item>();
+
+    private bool full = false;
+
+
+
+    private void InitalizeInventory()
     {
-        
+        inventoryItems = new List<Item>(); //inventoryItems = new Item[InventorySize];
+        QuickItems = new List<Item>(); //QuickItems = new List(4); //set size of quick items to 4
+
+        for (int i = 0; i < InventorySize; i++)
+        {
+            inventoryItems.Add(new Item("Empty", -1));
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            QuickItems.Add(new Item("Empty", -1));
+        }
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddToInventory(int HowMany, GameObject NewItem)
     {
-        
+        bool added = false;
+
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].name == NewItem.name)
+            {
+                //increase quantity of inventoryItems at i 
+            }
+        }
+        if (!added)
+        {
+
+        }
     }
+
+    private void RemoveFromInventory(int HowMany, string ItemName)
+    {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].Name.Equals(ItemName))
+            {
+                //finish
+            }
+        }
+        for (int i = 0; i < QuickItems.Count; i++)
+        {
+            if (QuickItems[i].Quantity == 0)
+            { 
+                //finish}
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        InitalizeInventory();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            showInventoryMenu = (showInventoryMenu) ? false : true;
+        }
+
+        shootKeys();
+    }
+
+    private void shootKeys()
+    {
+        if (Input.GetKeyDown("1") || Input.GetKeyDown("[1]"))
+            useItem(QuickItems[0].Name);
+        if (Input.GetKeyDown("2") || Input.GetKeyDown("[2]"))
+            useItem(QuickItems[2].Name);
+        if (Input.GetKeyDown("3") || Input.GetKeyDown("[3]"))
+            useItem(QuickItems[3].Name);
+        if (Input.GetKeyDown("4") || Input.GetKeyDown("[4]"))
+            useItem(QuickItems[4].Name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        int inventoryAmount = 1;
+        if (other.gameObject.tag.Equals("inventoryItem"))
+        {
+            if (inventoryItems[11].Name != "Empty")
+            {
+                full = true;
+                return;
+            }
+            if (other.gameObject.name.Contains("Group"))
+            {
+                inventoryAmount = other.gameObject.GetComponent<GroupInventoryAmount>().amount;
+            }
+
+            AddToInventory(inventoryAmount, other.gameObject);
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    //use items method
+        //specifics for health/potions 
+        //else for other items
+
+
+
+    //change quick items method
 }
--- Quiroz_K_P2/Assets/Scripts/Item.cs
+++ Quiroz_K_P2/Assets/Scripts/Item.cs
@@ -1,16 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+//does not have a monobehavior
+public class Item 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] int quantity;
+    [SerializeField] GameObject go;
+    public string name;
+
+    public Item(int quantity, GameObject gObject)
     {
-        
+        this.quantity = quantity;
+        go = gObject;
+       name = gObject.name;
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject GObject
     {
-        
+        get { return go; }
+        set { go = value; }
     }
+
+    public int Quantity
+    {
+        get { return quantity; }
+        set { quantity = value; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public override string ToString()
+    {
+        return go.name + ": " + quantity + "\n";
+    }
+
 }
